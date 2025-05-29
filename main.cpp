640611f226bde71da204fa58470968aab5ba2a91
nthday-jpg <24127166@student.hcmus.edu.vn>
2025-05-29 17:51:59 +0700

    Sua file main

--- main.cpp
+++ main.cpp
@@ -1,38 +1,7 @@
-﻿#include <iostream>
-#include <entt/entt.hpp>
-#include "src/systems/CollisionSystem.h"
-#include "src/components/EntityTag.h"
-#include "src/components/hitbox.h"
-#include "src/components/movementComponents.h"
-#include <SFML/Graphics.hpp>
+﻿#include "src/core/Game.h"
 
 int main() {
-    entt::registry registry;
-    entt::dispatcher dispatcher;
-    CollisionSystem collisionSystem(registry, dispatcher);
-
-    // Create player entity
-    auto player = registry.create();
-    registry.emplace<PlayerTag>(player);
-    registry.emplace<Position>(player, Position( 0.0f, 0.0f ));
-    registry.emplace<Hitbox>(player, Hitbox(10.0f, 10.0f, 0.0f, 0.0f));
-    auto enemy = registry.create();
-    registry.emplace<EnemyTag>(enemy);
-    registry.emplace<Position>(enemy, Position(10.0f, 10.0f));
-    registry.emplace<Hitbox>(enemy, Hitbox(10.0f, 10.0f, 10.0f, 10.0f));
-
-    auto wall = registry.create();
-    registry.emplace<WallTag>(wall);
-    registry.emplace<Position>(wall, Position( 5.0f, 5.0f ));
-    registry.emplace<Hitbox>(wall, Hitbox(10.0f, 10.0f, 5.0f, 5.0f));
-
-    auto spell = registry.create();
-    registry.emplace<SpellTag>(spell);
-    registry.emplace<Position>(spell, Position(10.0f, 5.0f));
-    registry.emplace<Hitbox>(spell, Hitbox(10.0f, 10.0f, 10.0f, 5.0f));
-
-    collisionSystem.update();
-
-
+	Game game;
+	game.run();
     return 0;
 }
