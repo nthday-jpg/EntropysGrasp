fea1198b6e60163cb0a464e8a9971e3070055bdd
DUY LÊ MINH <24127027@student.hcmus.edu.vn>
2025-06-04 18:36:05 +0700

    entity behavior

--- src/components/Spell.h
+++ src/components/Spell.h
@@ -19,52 +19,6 @@ enum class SpellBehaviorType {
 	OrbitMovement
 };
 
-struct StraightMovement {};
-struct HomingMovement { entt::entity target; };
-struct OrbitMovement { entt::entity center; float radius; float speed; };
-
-void straightMovementBehavior(entt::entity spell, entt::registry& registry, float dt) {
-	auto& position = registry.get<Position>(spell);
-	auto& velo = registry.get<Velocity>(spell);
-	auto& spellData = registry.get<SpellData>(spell);
-	auto& direction = registry.get<MovementDirection>(spell);
-
-	velo.x = direction.x * spellData.speed; 
-	velo.y = direction.y * spellData.speed;
-	position += velo * dt;
-}
-
-void homingMovementBehavior(entt::entity spell, entt::entity enemy, entt::registry& registry, float dt) {
-	auto& position = registry.get<Position>(spell);
-	auto& velo = registry.get<Velocity>(spell);
-	auto& spellData = registry.get<SpellData>(spell);
-	auto& enemyPosition = registry.get<Position>(enemy);
-
-    // Calculate direction towards the enemy
-    auto direction = enemyPosition - position;
-    float distance = sqrt(pow(direction.x, 2) + pow(direction.y, 2));
-    
-    if (distance > 0) {
-        direction /= distance; // Normalize the direction vector
-		velo.x = direction.x * spellData.speed;
-        velo.y = direction.y * spellData.speed;
-    }
-	position += velo * dt; // Move the spell towards the enemy
-}
-
-void orbitMovementBehavior(entt::entity entity, entt::registry& registry, float dt) {
-	auto& position = registry.get<Position>(entity);
-	auto& spellData = registry.get<SpellData>(entity);
-	auto& orbit = registry.get<OrbitMovement>(entity);
-	auto& centerPosition = registry.get<Position>(orbit.center);
-	// Calculate the angle of the orbit
-	float angle = dt * orbit.speed;
-	
-	// Update position based on orbit radius and angle
-	position.x = centerPosition.x + orbit.radius * cos(angle);
-	position.y = centerPosition.y + orbit.radius * sin(angle);
-}
-
 struct SpellData {
     std::string id;
 	float damage;
