4791b795ec7b9beda3a67b43351e8d633bedaef3
DUY LÊ MINH <24127027@student.hcmus.edu.vn>
2025-06-07 18:25:17 +0700

    fix

--- src/systems/CombatSystem.h
+++ src/systems/CombatSystem.h
@@ -10,7 +10,6 @@ class CombatSystem {
 	entt::dispatcher& dispatcher;
 public:
 	void handleCollision(std::vector<CollisionEvent>& collisionEvents);
-	void resolveEnemySpellCollision();
 	void applyDamage(float dmg, entt::entity);
 	void handlePlayerEnemyCollision(entt::entity player, entt::entity enemy);
 	void handleEnemySpellCollision(entt::entity enemy, entt::entity spell, SpellLibrary spellLibrary);
